c403ad0b1bf1fb1e374d6da0af003962e77ae519
TemitopeEmokpae <temokpae@oswego.edu>
2023-11-01 08:47:47 -0400

    updated server function

--- go.mod
+++ go.mod
@@ -2,12 +2,7 @@ module temokpae_server
 
 go 1.21.3
 
-require (
-	github.com/aws/aws-sdk-go v1.46.6
-	github.com/jamespearly/loggly v0.0.0-20220929110912-1d839b0715fa
-)
-
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require github.com/jamespearly/loggly v0.0.0-20220929110912-1d839b0715fa
 
 require (
 	github.com/TwiN/go-color v1.4.1 // indirect
--- main.go
+++ main.go
@@ -3,55 +3,19 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"time"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 
 	"github.com/jamespearly/loggly"
 
 	"github.com/gorilla/mux"
 )
 
-// Define a struct to store the collected data
-type APIData struct {
-	InternalName       string `json:"internalName"`
-	Title              string `json:"title"`
-	MetacriticLink     string `json:"metacriticLink"`
-	DealID             string `json:"dealID"`
-	StoreID            string `json:"storeID"`
-	GameID             string `json:"gameID"`
-	SalePrice          string `json:"salePrice"`
-	NormalPrice        string `json:"normalPrice"`
-	IsOnSale           string `json:"isOnSale"`
-	Savings            string `json:"savings"`
-	MetacriticScore    string `json:"metacriticScore"`
-	SteamRatingText    string `json:"steamRatingText"`
-	SteamRatingPercent string `json:"steamRatingPercent"`
-	SteamRatingCount   string `json:"steamRatingCount"`
-	SteamAppID         string `json:"steamAppID"`
-	ReleaseDate        int    `json:"releaseDate"`
-	LastChange         int    `json:"lastChange"`
-	DealRating         string `json:"dealRating"`
-	Thumb              string `json:"thumb"`
-}
-
 // Define a struct to store the server time
 type resTime struct {
 	SystemTime string
-}
-
-// Define a struct to store the table status
-type TableStatus struct {
-	Table string `json:"table"`
-	Count *int64 `json:"recordCount"`
 }
 
 // Define a struct to store the logging response
@@ -68,90 +32,6 @@ func ServerHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	sysTime := resTime{time.Now().String()}
 	json.NewEncoder(w).Encode(sysTime)
-}
-
-// All Handler function that displays all the items in DynamoDB
-func AllHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	client := loggly.New("LOGGLY_TOKEN")
-	client.EchoSend("info", "/all endpoint called")
-	w.WriteHeader(http.StatusOK)
-
-	// Initialize a session
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Region: aws.String("us-east-1"),
-		},
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-
-	// Create DynamoDB client
-	svc := dynamodb.New(sess)
-
-	var allResponse []APIData
-
-	// Scan the DB for all the items
-	scanErr := svc.ScanPages(&dynamodb.ScanInput{
-		TableName: aws.String("test-table-temokpae"),
-	}, func(page *dynamodb.ScanOutput, last bool) bool {
-		recs := []APIData{}
-
-		err := dynamodbattribute.UnmarshalListOfMaps(page.Items, &recs)
-		if err != nil {
-			panic(fmt.Sprintf("Failed to unmarshal Dynamodb Scan Items, %v", err))
-		}
-
-		allResponse = append(allResponse, recs...)
-
-		return true
-	})
-
-	// DB scanning error response
-	if scanErr != nil {
-		client.EchoSend("error", "Got error scanning DB: "+scanErr.Error())
-		os.Exit(1)
-	}
-
-	// Response of JSON
-	json.NewEncoder(w).Encode(allResponse)
-}
-
-// Status Handler function that displays the table status
-func StatusHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	client := loggly.New("LOGGLY_TOKEN")
-	client.EchoSend("info", "/status endpoint called")
-	w.WriteHeader(http.StatusOK)
-
-	// Initialize aws session
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Region: aws.String("us-east-1"),
-		},
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-
-	// Create DynamoDB client
-	svc := dynamodb.New(sess)
-
-	// Describe the table
-	input := &dynamodb.DescribeTableInput{
-		TableName: aws.String("test-table-temokpae"),
-	}
-
-	result, err := svc.DescribeTable(input)
-	if err != nil {
-		client.EchoSend("error", "Got error describing table: "+err.Error())
-		os.Exit(1)
-	}
-
-	// Create a response struct to be turned into JSON
-	var statusResponse TableStatus
-	statusResponse.Table = "test-table-temokpae"
-	statusResponse.Count = result.Table.ItemCount
-
-	// JSON Response
-	json.NewEncoder(w).Encode(statusResponse)
 }
 
 // Implement the http.ResponseWriter interface
@@ -173,8 +53,6 @@ func logglyMiddleware(next http.Handler) http.Handler {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/temokpae/all", AllHandler).Methods("GET")
-	router.HandleFunc("/temokpae/status", StatusHandler).Methods("GET")
 	router.HandleFunc("/temokpae/server", ServerHandler).Methods("GET")
 	log.Println("Server running...")
 	wrappedRouter := logglyMiddleware(router)
